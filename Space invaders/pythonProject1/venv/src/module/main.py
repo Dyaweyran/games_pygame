2c0b3f4f1b13068af2883c42481bc173bcf1d129
Иван Медведев <ivanmedvedev1998@gmail.com>
2025-02-09 16:52:14 +0300

    I have some troubles, need to refactor it

--- Space invaders/pythonProject1/venv/src/module/main.py
+++ Space invaders/pythonProject1/venv/src/module/main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 
 def player_update():
@@ -17,6 +18,7 @@ def bullet_create():
     x = player_x + player_width // 2 - bullet_width // 2
     y = player_y - bullet_height
     dy = -bullet_speed
+    sound_bullet.play()
     return x, y, dy
 
 
@@ -29,9 +31,42 @@ def bullet_update():
         bullet_alive = False
 
 
+def enemy_create():
+    """Create random coordinates and speed to create an enemy"""
+
+    x = random.randrange(0, display_width - enemy_width)
+    y = enemy_y_gap
+
+    dx = random.randrange(-1, 2) / 2
+    dy = random.randrange(1, 2) / 2
+
+    return x, y, dx, dy
+
+
+def check_collision():
+    global bullet_alive, enemy_alive
+    bullet_rect = pg.Rect(bullet_x, bullet_y, bullet_width, bullet_height)
+    enemy_rect = pg.Rect(enemy_x, enemy_y, enemy_width, enemy_height)
+    if bullet_rect.colliderect(enemy_rect):
+        bullet_alive = False
+        enemy_alive = False
+
+
+def enemy_update():
+    global enemy_x, enemy_y, enemy_dx, enemy_dy
+    enemy_x += enemy_dx
+    enemy_y += enemy_dy
+
+    if enemy_x < 0 \
+            or enemy_x + enemy_width > display_width \
+            or enemy_y + enemy_height > display_height:
+        enemy_x, enemy_y, enemy_dx, enemy_dy = enemy_create()
+
+
 def model_update():
     player_update()
     bullet_update()
+    enemy_update()
 
 
 def event_bullet(event):
@@ -49,6 +84,16 @@ def display_redraw():
     display.blit(player_img, (player_x, player_y))
     if bullet_alive:
         display.blit(bullet_img, (bullet_x, bullet_y))
+
+    if enemy_alive:
+        display.blit(enemy_img, (enemy_x, enemy_y))
+
+    score_surface = font.render(f'Score: 123', True, 'red')
+    display.blit(score_surface, (10, 10))
+
+    other_surface = restart_font.render('Game Over', True, (255, 255, 255))
+    w = other_surface.get_width()
+    display.blit(other_surface, ((display_width - w) / 2, display_height / 2))
 
     pg.display.update()
 
@@ -115,8 +160,33 @@ bullet_x, bullet_y, bullet_dy = 0, 0, 0
 bullet_speed = 1
 
 
+enemy_img = pg.image.load('resources/img/enemy.png')
+enemy_width = enemy_img.get_width()
+enemy_height = enemy_img.get_height()
+enemy_y_gap = 10
+enemy_x, enemy_y, enemy_dx, enemy_dy = enemy_create()
+enemy_alive = False
+
+
+font = pg.font.Font('resources/04B_19__.TTF', 32)
+high_score_font = pg.font.Font('resources/04B_19__.TTF', 64)
+
+restart_font = pg.font.SysFont('None', 48)
+
+pg.mixer.music.load('resources/audio/background.wav')
+pg.mixer.music.set_volume(0.3)
+pg.mixer.music.play(-1)
+
+sound_bullet = pg.mixer.Sound('resources/audio/laser.wav')
+sound_bullet.set_volume(0.5)
+
+
 running = True
 while running:
     model_update()
     display_redraw()
     running = event_process()
+    check_collision()
+    if not enemy_alive:
+        enemy_x, enemy_y, enemy_dx, enemy_dy = enemy_create()
+        enemy_alive = True
